b5e15418e0661b79eaad53484c6530ecfb9f6ee9
Kevin Leutzinger <6435727+kleutzinger@users.noreply.github.com>
2024-12-02 00:59:05 -0500

    more config

--- configure_nginx.sh
+++ configure_nginx.sh
@@ -29,6 +29,7 @@ for ((i = 0; i < ${#server_name_array[@]}; i++)); do
   server {
       listen ${PORT};
       server_name ${server_name_array[$i]};
+      client_max_body_size 5G;
 
       location / {
           auth_basic \"Restricted\";
@@ -36,13 +37,58 @@ for ((i = 0; i < ${#server_name_array[@]}; i++)); do
 
           proxy_set_header X-Real-IP \$remote_addr;
           proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+          proxy_set_header X-Forwarded-Proto \$scheme;
+          proxy_set_header Upgrade \$http_upgrade;
+          proxy_set_header Connection \$connection_upgrade;
           proxy_set_header Host \$http_host;
           proxy_set_header X-Nginx-Proxy true;
+          proxy_redirect off;
+          proxy_buffering off;
           proxy_http_version 1.1;
           proxy_pass ${proxy_pass_array[$i]};
       }
+
+      map \$http_upgrade \$connection_upgrade {
+        default upgrade;
+        '' close;
+      }
   }"
 done
+
+# http {
+#   server {
+#     listen 80;
+#     client_max_body_size 4G;
+
+#     server_name example.com;
+
+#     location / {
+#       proxy_set_header Host $http_host;
+#       proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+#       proxy_set_header X-Forwarded-Proto $scheme;
+#       proxy_set_header Upgrade $http_upgrade;
+#       proxy_set_header Connection $connection_upgrade;
+#       proxy_redirect off;
+#       proxy_buffering off;
+#       proxy_pass http://uvicorn;
+#     }
+
+#     location /static {
+#       # path for static files
+#       root /path/to/app/static;
+#     }
+#   }
+
+#   map $http_upgrade $connection_upgrade {
+#     default upgrade;
+#     '' close;
+#   }
+
+#   upstream uvicorn {
+#     server unix:/tmp/uvicorn.sock;
+#   }
+
+# }
 
 # End the http {} block and complete the nginx configuration string
 nginx_conf+="
